6e30323380144fe609c50bf8f58ff70373866b3e
idning <idning@gmail.com>
2014-06-30 23:28:38 +0800

    add tests

--- Makefile
+++ Makefile
@@ -1,2 +1,2 @@
 install :
-	gcc -g -Wall -fPIC --shared -o int64.so int64.c -I/usr/local/include
+	gcc -g -Wall -fPIC --shared -o int64.so int64.c -I /home/ning/idning-source/c/lua/lua-5.1/src/ /home/ning/idning-source/c/lua/lua-5.1/src/liblua.a -lm -ldl
--- test.lua
+++ test.lua
@@ -1,13 +1,13 @@
-lib = require "int64"
+int64 = require "int64"
 
-local int64 = lib.new
-print(lib.tostring(int64 "\1\2\3\4\5\6\7\8"))
+print(int64.tostring(int64.new "\1\2\3\4\5\6\7\8"))
 
-a = 1 + int64(1)
-b = int64 "\16" + int64("9",10)
-print(lib.tostring(a,10), lib.tostring(b,2))
+
+a = 1 + int64.new(1)
+b = int64.new "\16" + int64.new("9",10)
+print(int64.tostring(a,10), int64.tostring(b,2))
 print("+", a+b)
-print("-", lib.tostring(a-b,10))
+print("-", int64.tostring(a-b,10))
 print("*", a*b)
 print("/", a/b)
 print("%", a%b)
@@ -15,3 +15,11 @@ print("^", a^b)
 print("==", a == b)
 print(">", a > b)
 print("#", #a)
+
+x = int64.new('9223372036854775806', 10)
+               
+print(int64.tostring(x, 10))
+x = x+1
+print(int64.tostring(x, 10))
+
+
